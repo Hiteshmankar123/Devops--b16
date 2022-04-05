e61ab87e6af0ad3287f13cb9d88fc3e6e3a73f34
Hitesh Mankar <Hiteshmankar777@gmail.com>
2022-04-05 18:17:28 +0530

    --- Auto-Git Commit ---

---
+++ Pipeline/seedjob2.groovy
@@ -0,0 +1,17 @@
+pipelineJob('scripted-pipeline_converted') {
+  
+  description('')
+  
+  displayName('scripted-pipeline_converted')
+  
+  keepDependencies(false)
+  
+  quietPeriod(0)
+  
+  checkoutRetryCount(0)
+  
+  disabled(false)
+  
+  concurrentBuild(false)
+  
+}
